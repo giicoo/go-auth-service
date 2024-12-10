e1b35ebe0624b67bfa1a4ed77c18dac6c312664e
giicoo <arnst.artyom@gmail.com>
2024-12-11 00:44:51 +0300

    configs

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var cfg Config
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
